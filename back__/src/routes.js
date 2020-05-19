bf1481597e0a5846386992b8a9bc760bac324345
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-05-19 11:07:03 -0300

    Rearranging the code

--- back__/src/routes.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express')
-const routes = Router()
-
-routes.get('/', (req, res) => res.json({ message: 'Hello' }))
-
-module.exports = routes()
--- back__/src/server.js
+++
@@ -1,3 +0,0 @@
-const server = require('./app')
-
-server.listen(3333, () => console.log('Server running:3333'))
