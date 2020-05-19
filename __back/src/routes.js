3d41e8534cc62bad8e3dd443322df2533b210425
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-05-19 16:26:26 -0300

    Add validations

--- __back/src/routes.js
+++ __back/src/routes.js
@@ -1,9 +1,45 @@
 const { Router } = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 const routes = Router()
 
 const UserController = require('./controllers/UserController')
 
 routes.get('/', UserController.index)
-routes.get('/register', UserController.create)
+
+routes.post(
+  '/register',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      user: Joi.string().required(),
+      email: Joi.string.required(),
+      password: Joi.string().required()
+    })
+  }),
+  UserController.create
+)
+
+routes.put(
+  '/user/id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().integer().required()
+    }),
+
+    [Segments.BODY]: Joi.object().keys({
+      user: Joi.string().required()
+    })
+  }),
+  UserController.update
+)
+
+routes.delete(
+  '/user/id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().integer().required()
+    })
+  }),
+  UserController.delete
+)
 
 module.exports = routes
