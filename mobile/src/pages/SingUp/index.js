2c4a398d28cfc68e77c6ead8474954b7b3c133eb
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-05-21 15:23:42 -0300

    Register working

--- mobile/src/pages/SingUp/index.js
+++ mobile/src/pages/SingUp/index.js
@@ -1,21 +1,43 @@
 import React, {useState} from 'react';
 import {Container, Box, Field, Button, Text} from '../../styled/singup';
+import api from '../../service/api';
 
 export default function SingUp() {
   const [user, setUser] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleSubmit() {}
+  async function handleSubmit() {
+    const data = {
+      user,
+      email,
+      password,
+    };
+
+    const token = await api.post('/register', data);
+    console.log(token.message);
+  }
 
   return (
     <Container>
       <Box>
-        <Field placeholder="user" />
-        <Field placeholder="example@example.com" />
-        <Field placeholder="password" />
+        <Field
+          placeholder="user"
+          value={user}
+          onChangeText={text => setUser(text)}
+        />
+        <Field
+          placeholder="example@example.com"
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <Field
+          placeholder="password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
 
-        <Button>
+        <Button onPress={handleSubmit}>
           <Text>Register</Text>
         </Button>
       </Box>
